7950eec13682992795b5e574cccd25b8ed657c63
Balázs Szalatnyai <szalatnyaibalazs@gmail.com>
2020-11-23 11:51:51 +0100

    Első 5 feladat megoldáas

--- Morze2.0/Program.cs
+++ Morze2.0/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace Morze2._0
 {
@@ -11,7 +12,7 @@ namespace Morze2._0
     {
         static Dictionary<string, string> abcmorze = new Dictionary<string, string>();
         static Dictionary<string, string> morzeabc = new Dictionary<string, string>();
-
+        static List<Szoveg> idezetek = new List<Szoveg>();
         static void abcbeolvasas()
         {
             StreamReader be = new StreamReader("morzeabc.txt");
@@ -28,10 +29,47 @@ namespace Morze2._0
         {
             Console.WriteLine($"3. feladat: {morzeabc.Count} db karakter kódját tartalmazza");
         }
+        static void Negyedik()
+        {
+            Console.Write("4. Feladat: Kérek egy karakert: ");
+            string betu = Console.ReadLine();
+
+            if (abcmorze.ContainsKey(betu))
+            {
+                Console.WriteLine($"\tA {betu} karaketer morzekódja: {abcmorze[betu]}");
+            }
+            else
+            {
+                Console.WriteLine("Nem található a kódtárban ilyen karakter");
+            }
+        }
+        static void Otodik()
+        {
+
+        }
+        static string morze2szöveg(string kodolt)
+        {
+            StringBuilder vissza = new StringBuilder();
+            string[] szavak = kodolt.Replace("       ", ";").Split(';');
+            foreach (var szo in szavak)
+            {
+                string[] betuk = szo.Trim().Replace("   ", ";").Split(';');
+
+                foreach (var betu in betuk)
+                {
+                    vissza.Append(morzeabc[betu]);
+                }
+                vissza.Append(" ");
+            }
+
+            return vissza.ToString().Trim();
+
+        }
         static void Main(string[] args)
         {
             abcbeolvasas();
             Harmadik();
+            Negyedik();
 
             Console.ReadKey();
         }
---
+++ Morze2.0/Szoveg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morze2._0
+{
+    class Szoveg
+    {
+        private string szerzo;
+        public string Szerzo { get { return szerzo; } }
+        private string idezet;
+        public string Idezet { get { return idezet; } }
+        private int hossz;
+        public int Hossz { get { return hossz; } }
+
+        public Szoveg(string szerzo,string idezet)
+        {
+            this.szerzo = szerzo;
+            this.idezet = idezet;
+            hossz = this.idezet.Length;
+        }
+    }
+}
